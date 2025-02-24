1082227319b1e2ed9e18c6749d4f1c28de094270
Yogesh M V <yogeshmv74@gmail.com>
2025-02-24 17:11:13 +0530

    Degraded model to 8B

--- llama_model.py
+++ llama_model.py
@@ -1,15 +1,15 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-model_id = "meta-llama/Llama-3.3-70B-Instruct"
-quantization_config = BitsAndBytesConfig(load_in_8bit=True)
+model_id = "meta-llama/Llama-3.1-8B-Instruct"
+quantization_config = None
 
 quantized_model = AutoModelForCausalLM.from_pretrained(
-    model_id, device_map="auto", torch_dtype=torch.bfloat16, quantization_config=quantization_config)
+    model_id, torch_dtype=torch.float32, quantization_config=quantization_config)
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 input_text = "What are we having for dinner?"
-input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+input_ids = tokenizer(input_text, return_tensors="pt")
 
 output = quantized_model.generate(**input_ids, max_new_tokens=10)
 
