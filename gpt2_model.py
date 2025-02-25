c48ddf97e57ef1190b44e2ed6bd7c47cde3ce359
Yogesh M V <yogeshmv74@gmail.com>
2025-02-25 10:22:56 +0530

    Done few changes to the benchmarking part

--- gpt2_model.py
+++ gpt2_model.py
@@ -4,10 +4,12 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import time
 
 # Load the Tokenizer & Model
-
-tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2")
+tokenizer = AutoTokenizer.from_pretrained("openai-community/gpt2", padding_side='left')
 model = AutoModelForCausalLM.from_pretrained("openai-community/gpt2")
 tokenizer.pad_token = tokenizer.eos_token
+device = "cpu"  # device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+max_new_token = 10
 
 # Input prompt as batch
 sentences = [
@@ -23,17 +25,30 @@ sentences = [
     "He went to the gym every day, determined to improve"
 ]
 
+
+
 num_sentences = len(sentences)
 
+# tokenization and  it's time
+enc_start = time.time()
 encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
-device = "cpu"  # device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
 encoded_input = {k: v.to(device) for k, v in encoded_input.items()}
+enc_time = time.time() - enc_start
 
+input_token = encoded_input["input_ids"].numel()
+# # warmup run
+# with torch.no_grad():
+#     _ = model.generate(
+#     **encoded_input,
+#     max_new_tokens=max_new_token,
+#     num_beams=4,
+#     return_dict_in_generate=True,
+#     output_scores=True,
+# )
 
 ttft_start = time.time()
 with torch.no_grad():
-    _ = model.generate(
+    outputs = model.generate(
     **encoded_input,
     max_new_tokens=1,
     num_beams=4,
@@ -46,7 +61,7 @@ start_time = time.time()
 with torch.no_grad():
     outputs = model.generate(
     **encoded_input,
-    max_new_tokens=10,
+    max_new_tokens=max_new_token,
     num_beams=4,
     do_sample = True,
     temperature = 0.7,
@@ -56,23 +71,44 @@ with torch.no_grad():
 )
 end_time = time.time()
 
-print(outputs)
-
+decode_start = time.time()
 generated_texts = [tokenizer.decode(output, skip_special_tokens=True) for output in outputs.sequences]
+decode_time = time.time() - decode_start
+# print(outputs)
 num_tokens_generated = sum(len(output) for output in outputs.sequences)
 
-
-total_time = (end_time - start_time) * 1000
-
-latency = total_time / num_sentences
 ttft_time = (ttft_end - ttft_start)
-tpot_time = ((total_time - ttft_time) / 1000) / num_tokens_generated 
-tps = num_tokens_generated / (latency / 1000)
-rps = num_sentences / latency
-print(f"Latency: {latency:.4f} ms")
+tpot_time = ((end_time - start_time) - ttft_time) / (num_tokens_generated - 1)
+
+total_time = enc_time + (ttft_time + tpot_time) +decode_time
+
+# need to remove input tokens from output tokens
+num_tokens_generated -= input_token
+
+print(f"Number of Tokens generated:{num_tokens_generated}") # 210
+print(f"Number of sentences: {num_sentences}") # 10
+
+latency_perBatch = total_time / num_sentences # per-batch = (tot_time/ num_itr)
+latency = total_time # entire batch = tot_time
+tps = num_tokens_generated / (latency / 1000) 
+rps = num_sentences / (latency / 1000)
+
+# cross verify latency, ttft, tpot using the below formula
+crossVerify_latency = ttft_time + (tpot_time * (max_new_token - 1))
+if (crossVerify_latency == latency - enc_time - decode_time):
+    print("Correct Latency") # if correct 
+else:
+    print("Incorrect Latency")
+print()
+
+
+# print Preformance measures    
+# print(f"Input tokens: {input_token}")
+print(f"CVLatency: {crossVerify_latency * 1000:.4f} ms")
+print(f"Latency: {latency * 1000:.4f} ms")
 print(f"TTFT: {ttft_time:.4f} s")
 print(f"TPOT: {tpot_time:.4f} tps")
 print(f"TPS: {tps:.4f} tps")
-print(f"RPS: {tps:.4f} rps")
+print(f"RPS: {rps:.4f} rps")
 
 
